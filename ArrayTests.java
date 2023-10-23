b7dafdedfefd46dd21a3b8578593c58ed2db482f
melon_g <135576306+graceswang@users.noreply.github.com>
2023-10-23 11:39:51 -0700

    commit to reverseInPlace and reversed
    The reverseInPlace was changing the original array first and exacting the element from the array, so it will be duplicated. I create a new array as the medium and make a deep copy of it to the old array. For reversed method, it returns the unchanged array instead of the new array.

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2() {
+    int[] input2 = { 3,4 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 4,3 }, input2);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input2 = { 3,4 };
+    assertArrayEquals(new int[]{4,3}, ArrayExamples.reversed(input2));
   }
 }
